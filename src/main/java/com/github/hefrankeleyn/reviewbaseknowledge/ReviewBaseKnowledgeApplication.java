64ad0847f15bb0e5c99ba647af2c96156ea0de9e
Li Fei <hefrankeleyn@gamil.com>
2025-01-17 21:09:53 +0800

    初始化项目

---
+++ src/main/java/com/github/hefrankeleyn/reviewbaseknowledge/ReviewBaseKnowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.hefrankeleyn.reviewbaseknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewBaseKnowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewBaseKnowledgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hefrankeleyn/reviewbaseknowledge/ReviewBaseKnowledgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hefrankeleyn.reviewbaseknowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewBaseKnowledgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
